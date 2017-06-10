53eb3318647b92a3860c163f4e631d0883a0138a
guilhermePaciulli <gpaciulli100@gmail.com>
2017-06-10 20:20:08 -0300

    Database classes

---
+++ MiniChallenge3/Achievement+CoreDataProperties.swift
@@ -0,0 +1,23 @@
+//
+//  Achievement+CoreDataProperties.swift
+//  MiniChallenge3
+//
+//  Created by Guilherme Paciulli on 09/06/17.
+//  Copyright © 2017 Guilherme Paciulli. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+extension Achievement {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Achievement> {
+        return NSFetchRequest<Achievement>(entityName: "Achievement")
+    }
+
+    @NSManaged public var amount: Int16
+    @NSManaged public var assetIdentifier: String?
+    @NSManaged public var hasAchievement: Bool
+
+}
