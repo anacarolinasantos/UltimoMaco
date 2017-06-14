c03c0f612df77da6865333a5f2154e929d0b84c7
OsnielLopes <niellt@hotmail.com>
2017-06-14 18:19:28 -0300

    add historicviewcontroller

---
+++ MiniChallenge3/HistoricViewController.swift
@@ -0,0 +1,38 @@
+//
+//  HistoricViewController.swift
+//  MiniChallenge3
+//
+//  Created by Osniel Lopes Teixeira on 13/06/17.
+//  Copyright © 2017 Guilherme Paciulli. All rights reserved.
+//
+
+import UIKit
+
+class HistoricViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func voltarButtonPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
