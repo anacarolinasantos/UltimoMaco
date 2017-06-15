38e1049984371fd3f3c0792fa3b84e8ab6507a45
anacarolinasantos <ana.carolinasantos@icloud.com>
2017-06-15 12:51:14 -0300

    HealthInformation classes added

---
+++ MiniChallenge3/HealthInformationModel.swift
@@ -0,0 +1,9 @@
+//
+//  AssistanceModel.swift
+//  MiniChallenge3
+//
+//  Created by Ana Carolina Silva on 15/06/17.
+//  Copyright © 2017 Guilherme Paciulli. All rights reserved.
+//
+
+import Foundation
