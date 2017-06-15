fe9ecd5a537e0f6be89f0e532df28ee40f75b280
guilhermePaciulli <gpaciulli100@gmail.com>
2017-06-15 16:08:56 -0300

    Achievements show instantly at load screen - with bugs

---
+++ MiniChallenge3/MainTabBarController.swift
@@ -0,0 +1,39 @@
+//
+//  MainTabBarController.swift
+//  MiniChallenge3
+//
+//  Created by Guilherme Paciulli on 15/06/17.
+//  Copyright © 2017 Guilherme Paciulli. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+    var obtainedAchievements: [Achievement] = []
+    
+    var index: Int = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        obtainedAchievements = AchievementsController.checkAllAchievements()
+        index = obtainedAchievements.count
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        index -= 1
+        showAchievementsPopUp(all: obtainedAchievements, index: index)
+    }
+    
+    private func showAchievementsPopUp(all: [Achievement], index: Int) {
+        if index >= 0 {
+            if let popUp = self.storyboard?.instantiateViewController(withIdentifier: "popUp") as? AchievementPopUpViewController {
+                popUp.achievement = all[index]
+                popUp.isLastPopUpView = index == 0
+                present(popUp, animated: true, completion: nil)
+            }
+        }
+    }
+
+}
