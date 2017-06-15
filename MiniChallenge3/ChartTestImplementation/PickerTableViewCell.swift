5737a768183bafc3895c166426abf24d8e4073ab
OsnielLopes <niellt@hotmail.com>
2017-06-15 16:16:31 -0300

    Add PickerTableViewCell class

---
+++ MiniChallenge3/ChartTestImplementation/PickerTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PickerTableViewCell.swift
+//  MiniChallenge3
+//
+//  Created by Osniel Lopes Teixeira on 15/06/17.
+//  Copyright © 2017 Guilherme Paciulli. All rights reserved.
+//
+
+import UIKit
+
+class PickerTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
