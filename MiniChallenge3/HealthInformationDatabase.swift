e40c000cf73f51eb8f6e8f453a1cc14783a1fd10
anacarolinasantos <ana.carolinasantos@icloud.com>
2017-06-16 19:46:41 -0300

    Health information database completed

--- MiniChallenge3/HealthInformationDatabase.swift
+++ MiniChallenge3/HealthInformationDatabase.swift
@@ -10,21 +10,50 @@ import UIKit
 
 
 class HealthInformationDB {
-    // -- Hábitos
-//    static let healthInformations: [HealthInformationModel]?
+    static let healthInformations: [HealthInformationModel] = [
+        //Habits informations
+        HealthInformationModel.init(category: .habits, information: "Evite trocar o vício do cigarro por outros! Não desconte a vontade de cigarro em alimentos gordurosos ou em bebidas como café ou cerveja."),
+        HealthInformationModel.init(category: .habits, information: "Mantenha hábitos saudáveis: pratique exercícios físicos e coma de forma regular e sem exageros, isto irá te ajudar nos momentos de fissura."),
+        HealthInformationModel.init(category: .habits, information: "Mude a rotina, seus hábitos. Muito do vício pelo cigarro vem de uma rotina, de apenas um hábito. Procure fazer algo pelas manhãs como caminhar ou exercitar algum hobbie."),
+        HealthInformationModel.init(category: .habits, information: "Evite comer no lugar de fumar, isto é a principal causa de muitos ex-fumantes engordarem no processo de parar de fumar."),
+        HealthInformationModel.init(category: .habits, information: "Tente adiar os cigarros da manhã, fazendo isto, você vai fumar cada vez menos e terá menos dificuldade de parar depois, pois os cigarros da manhã, normalmente, são os mais difícies de parar."),
+        //Health informations
+        HealthInformationModel.init(category: .health, information: "Fumar pode causar inúmeras doenças além de diminuir muito do seu potencial diário em termos de cansaço, estresse, atividade, entre outros."),
+        HealthInformationModel.init(category: .health, information: "Já se sentiu cansado por apenas subir uma escada? Pois é, o fumo diminui muito seu fôlego e sua capacidade respiratória."),
+        HealthInformationModel.init(category: .health, information: "O alívio do estresse pelo cigarro é compensado com um estresse ainda maior, isto pode prejudicar seu desempenho na escola, faculdade, trabalho, etc."),
+        HealthInformationModel.init(category: .health, information: "Se você parar de fumar agora: em 20 minutos, sua pressão arterial e a frequência do pulso voltam ao normal."),
+        HealthInformationModel.init(category: .health, information: "Se você parar de fumar agora: em 2 horas, não haverá mais nicotina no seu sangue"),
+        HealthInformationModel.init(category: .health, information: "Se você parar de fumar agora: em 8 horas, os níveis de monóxido de carbono no sangue chegam aos valores normais e o nível de oxigênio aumenta."),
+        HealthInformationModel.init(category: .health, information: "Se você parar de fumar agora: em 24 horas, os pulmões já funcionam melhor e os brônquios começam a limpar os resíduos deixados pelo fumo."),
+        HealthInformationModel.init(category: .health, information: "Se você parar de fumar agora: em 48 horas, o olfato já percebe melhor os cheiros e o paladar já degusta melhor a comida."),
+        HealthInformationModel.init(category: .health, information: "Se você parar de fumar agora: dentro de 2 semanas a 3 meses, a circulação sanguínea melhora consideravelmente. Caminhar torna-se mais fácil e a função pulmonar melhora em até 30%."),
+        HealthInformationModel.init(category: .health, information: "Se você parar de fumar agora: em 1 ano, o risco de doenças ligadas a males do coração, como infarto, cai pela metade."),
+        HealthInformationModel.init(category: .health, information: "Se você parar de fumar agora: em 5 anos, diminui o risco de acidente vascular cerebral (AVC). Já o risco de infarto do miocárdio e morte por ataque cardíaco se aproxima daquele de quem nunca fumou."),
+         HealthInformationModel.init(category: .health, information: "Se você parar de fumar agora: em 10 anos, o risco de sofrer um infarto será igual ao de quem nunca fumou. E haverá uma redução dos riscos de câncer na boca, garganta, esôfago, bexiga, rim e pâncreas."),
+         HealthInformationModel.init(category: .health, information: "Se você parar de fumar agora: em 15 anos, o risco de desenvolver câncer de pulmão pode igualar-se ao dos não fumantes."),
+         //Help informations
+        HealthInformationModel.init(category: .help, information: "O SUS oferece todo um sistema de apoio de grupos tabagistas para parar de fumar consulte mais pelos seguintes contatos: \nAcesse o site: http://www2.inca.gov.br/wps/wcm/connect/acoes_programas/site/home/nobrasil/programa-nacional-controle-tabagismo/tratamento-do-tabagismo"),
+        //Tips informations
+        HealthInformationModel.init(category: .tips, information: ""),
+        //Fissure informations
+        HealthInformationModel.init(category: .fissure, information: "Procure se distrair, acima de tudo, a vontade de fumar passa rápido em média de 5 a 10 minutos a ânsia pela nicotina já acaba."),
+        HealthInformationModel.init(category: .fissure, information: "Uma caminhada rápida pelo quarteirão pode ocupar esse tempo e deixar de lado essa vontade!"),
+        HealthInformationModel.init(category: .fissure, information: "Exercite o músculo da boca! Muitos fumantes dizem ter passado a vontade de fumar por apenas beberem água, comerem algo leve, chuparem uma bala, entre outros. Mas lembre-se de escolher opções saudáveis!"),
+        HealthInformationModel.init(category: .fissure, information: "Jogue algum joguinho no celular ou se distraía com um passatempo rápido, esse tipo de atividade ajuda a ocupar esse tempo."),
+        HealthInformationModel.init(category: .fissure, information: "Respire fundo, relaxe, pense em coisas boas, uma pequena meditação interna ajuda a passar a vontade."),
+        HealthInformationModel.init(category: .fissure, information: "Procure comer alguma coisa com um gosto forte, como gengibre ")]
     
-//    static func getRandomNotification() -> [String]{
-//        
-//        //Gets a random notification
-//        let randNotification = notifications[Int(arc4random_uniform(UInt32(notifications.count)))]
-//        
-//        //Gets each element of the notification
-//        let identifier = randNotification.identifier
-//        let title = randNotification.title
-//        let message = randNotification.message
-//        
-//        //Returns notification parameters in Array
-//        return [identifier, title, message]
-//    }
+    static func getInformation(from category: HealthInformationCategory) -> [HealthInformationModel]{
+        
+        var healthInformationsCategory = [HealthInformationModel]()
+        //Return an array with all the informations from this category
+        for information in healthInformations {
+            if information.category == category {
+                healthInformationsCategory.append(information)
+            }
+        }
+        
+        return healthInformationsCategory
+    }
     
 }
