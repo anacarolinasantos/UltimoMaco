23c8754a12a62afac17fdd44acbe3e8e49ac6b3b
OsnielLopes <niellt@hotmail.com>
2017-06-18 22:26:07 -0300

    Agora as notificacoes funcionam (90%)

--- MiniChallenge3/AppConfigurationsTableViewController.swift
+++ MiniChallenge3/AppConfigurationsTableViewController.swift
@@ -9,8 +9,10 @@
 import UIKit
 
 class AppConfigurationsTableViewController: UITableViewController {
-
+    
     //MARK: - Outlets
+    @IBOutlet weak var notificationSwitch: UISwitch!
+    @IBOutlet weak var informativeNotificationSwitch: UISwitch!
     @IBOutlet weak var pickerCell: UITableViewCell!
     @IBOutlet weak var datePickerOutlet: UIDatePicker!
     @IBOutlet weak var hourLabel: UILabel!
@@ -35,25 +37,37 @@ class AppConfigurationsTableViewController: UITableViewController {
     // -- SWITCH DID CHANGE VALUE
     @IBAction func notificationSwitchChangeValue(_ sender: UISwitch) {
         // -- NOTIFICAÇÃO LEMBRETE
-     
+        if sender.isOn{
+            let hour = Int((hourLabel.text?.substring(to: (hourLabel.text?.index((hourLabel.text?.startIndex)!, offsetBy: 2))!))!)
+            let minute = Int((hourLabel.text?.substring(from: (hourLabel.text?.index((hourLabel.text?.startIndex)!, offsetBy: 3))!))!)
+            
+            NotificationController.sendNotificationDaily(["lembreteNoturno","Boa noite!",
+                                                          "Não se esqueça de inserir toda a quantia de cigarros que você consumiu hoje."],
+                                                         Calendar.current.date(bySettingHour: hour!, minute: minute!, second: 0, of: Date())!)
+        }else{
+            NotificationController.center.removePendingNotificationRequests(withIdentifiers: ["lembreteNoturno"])
+        }
+    }
+    
+    @IBAction func informativeNotificationDidChangeValue(_ sender: UISwitch){
+        // -- NOTIFICAÇÕES INFORMATIVAS
         if sender.isOn {
             NotificationController.prepareAll(daysFromToday: LineChartData().getDaysUntilTheZeroPoint())
         }else{
             NotificationController.center.removeAllPendingNotificationRequests()
-            let hour = Int((hourLabel.text?.substring(to: (hourLabel.text?.index((hourLabel.text?.startIndex)!, offsetBy: 2))!))!)
-            let minute = Int((hourLabel.text?.substring(from: (hourLabel.text?.index((hourLabel.text?.startIndex)!, offsetBy: 3))!))!)
-
-            NotificationController.sendNotificationDaily(["lembreteNoturno","Boa noite!",
-                                                          "Não se esqueça de inserir toda a quantia de cigarros que você consumiu hoje."],
-                                                         Calendar.current.date(bySettingHour: hour!, minute: minute!, second: 0, of: Date())!)
+            
+            if informativeNotificationSwitch.isOn{
+                let hour = Int((hourLabel.text?.substring(to: (hourLabel.text?.index((hourLabel.text?.startIndex)!, offsetBy: 2))!))!)
+                let minute = Int((hourLabel.text?.substring(from: (hourLabel.text?.index((hourLabel.text?.startIndex)!, offsetBy: 3))!))!)
+                
+                NotificationController.sendNotificationDaily(["lembreteNoturno","Boa noite!",
+                                                              "Não se esqueça de inserir toda a quantia de cigarros que você consumiu hoje."],
+                                                             Calendar.current.date(bySettingHour: hour!, minute: minute!, second: 0, of: Date())!)
+            }
         }
         
     }
-
-    @IBAction func informativeNotificationDidChangeValue(_ sender: UISwitch){
-        // -- NOTIFICAÇÕES INFORMATIVAS
-        
-    }
+    
     @IBAction func pickerDidChangeValue(_ sender: UIDatePicker) {
         
         NotificationController.getPendingNotifications()
@@ -66,7 +80,8 @@ class AppConfigurationsTableViewController: UITableViewController {
             hourLabel.text = "\(datePicker.hour!):\(datePicker.minute!)"
         }
         
-        
+        if notificationSwitch.isOn{
         NotificationController.sendNotificationDaily(["lembreteNoturno","Boa noite!","Não se esqueça de inserir toda a quantia de cigarros que você consumiu hoje."], Calendar.current.date(bySettingHour:datePicker.hour!, minute: datePicker.minute!, second: 0, of: Date())!)
+        }
     }
 }
